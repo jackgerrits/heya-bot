cec0d1de1c035e7a21f756d7a8e6a80ab2c8da1c
Jack Gerrits <jackgerrits95@gmail.com>
2016-07-30 16:01:58 +1000

    Added intent handler object

---
+++ intentHandler.js
@@ -0,0 +1,32 @@
+function intentHandler() {
+    var handlers = [];
+
+    this.RESULTS = {
+        SUCCESS : "SUCCESS",
+        FAILURE : "FAILURE",
+        CANT_HANDLE : "CANT_HANDLE"
+    }
+
+    // Handler must be a funtion that accepts context and entities
+    this.registerHandler = function(handler){
+        handlers.push(handler);
+    }
+
+    this.handleRequest = function(context, entities) {
+        var result = this.RESULTS.CANT_HANDLE;
+
+        for(var i = 0; i < handlers.length; ++i){
+            result = handlers[i](context, entities);
+            if(result == this.RESULTS.FAILURE || result == this.RESULTS.SUCCESS){
+                return result;
+            }
+        }
+        // If this location is reached, there is no handler for this query
+        return this.RESULTS.CANT_HANDLE;
+    }
+
+    return this;
+}
+
+// Holds an instantiated intentHandler to be used by the app
+module.exports.intentHandler = intentHandler();
