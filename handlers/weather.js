2cfc2a60c217c47db99547a43c1e0bcd0aeed9b0
krusli <rusli.kenneth@gmail.com>
2016-07-31 00:24:34 +1000

    Add weather api sample call

--- handlers/weather.js
+++ handlers/weather.js
@@ -1,24 +1,34 @@
 var registry = require("../intentHandler.js");
 
-const ForecastIO = require('forecast-io')
-const forecast = new ForecastIO('') // TODO: remove API key
+const ForecastIO = require('forecast-io');
+const forecast = new ForecastIO('');
 
-/*
-forecast
-    .latitude('37.8267')
-    .longitude('-122.423')
-    .time('2016-01-28')
+const cities = require("../cities.json");
+
+var city = cities['Melbourne'];
+
+if (city) {
+  // Get weather forecast using Dark Sky API. Date is YYYY-MM-DD. All parameters
+  // are string types.
+  forecast
+    .latitude(city.lat.toString())
+    .longitude(city.lon.toString())
+    .time('2016-07-31')
     .units('ca')
     .language('en')
-    .exclude('minutely,daily')
+    .exclude('minutely,hourly')
     .get()
     .then(res => {
-        console.log(res)
+      console.log(res)
     })
     .catch(err => {
-        console.log(err)
+      console.log(err)
     })
-*/
+}
+else {
+  console.log("NOT_FOUND");
+}
+
 
 module.exports = function(context, entities, callback){
     var returnObject = {execStatus : "CANT_HANDLE", context : context};
