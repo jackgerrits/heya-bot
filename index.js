7c18d7dc3c4f6eedadaf215f70259a7ceb36ac77
Jack Gerrits <jackgerrits95@gmail.com>
2016-07-30 23:18:14 +1000

    Id testing

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.post('/webhook', function (req, res) {
           // Yay! We got a new message!
           // We retrieve the Facebook user ID of the sender
           const sender = event.sender.id;
-
+          console.log(event);
           // We retrieve the user's current session, or create one if it doesn't exist
           // This is needed for our bot to figure out the conversation history
           const sessionId = findOrCreateSession(sender);
