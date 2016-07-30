da91bad4d125dc88eb54476a09342bff7e007cc4
Jack Gerrits <jackgerrits95@gmail.com>
2016-07-30 19:08:47 +1000

    Should be good now

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
@@ -6,25 +8,24 @@ var intentHandler = require("./intentHandler.js");
 var Wit = require('node-wit').Wit;
 var log = require('node-wit').log;
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 // Wit.ai parameters
 var WIT_TOKEN = process.env.WIT_TOKEN;
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
+  res.send('This is TestBot Server');
 });
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
+  if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Invalid verify token');
+  }
 });
 
 // This will contain all user sessions.
@@ -35,7 +36,7 @@ var sessions = {};
 var findOrCreateSession = function(fbid) {
   var sessionId;
   // Let's see if we already have a session for the user fbid
-  Object.keys(sessions).forEach(function(k){
+  Object.keys(sessions).forEach(function (k) {
     if (sessions[k].fbid === fbid) {
       // Yep, got it!
       sessionId = k;
@@ -44,14 +45,17 @@ var findOrCreateSession = function(fbid) {
   if (!sessionId) {
     // No session found for user fbid, let's create a new one
     sessionId = new Date().toISOString();
-    sessions[sessionId] = {fbid: fbid, context: {}};
+    sessions[sessionId] = { fbid: fbid, context: {} };
   }
   return sessionId;
 };
 
 // Our bot actions
 var actions = {
-  send(sessionId, text) {
+  send: function send(_ref, _ref2) {
+    var sessionId = _ref.sessionId;
+    var text = _ref2.text;
+
     // Our bot has something to say!
     // Let's retrieve the Facebook user whose session belongs to
     var recipientId = sessions[sessionId].fbid;
@@ -59,70 +63,68 @@ var actions = {
       // Yay, we found our recipient!
       // Let's forward our bot response to her.
       // We return a promise to let our bot know when we're done sending
-      return sendMessage(recipientId, text)
-      .then(() => null)
-      .catch((err) => {
-        console.error(
-          'Oops! An error occurred while forwarding the response to',
-          recipientId,
-          ':',
-          err.stack || err
-        );
+      return sendMessage(recipientId, text).then(function () {
+        return null;
+      }).catch(function (err) {
+        console.error('Oops! An error occurred while forwarding the response to', recipientId, ':', err.stack || err);
       });
     } else {
       console.error('Oops! Couldn\'t find user for session:', sessionId);
       // Giving the wheel back to our bot
-      return Promise.resolve()
+      return Promise.resolve();
     }
   },
-  handleIntent({context, entities}) {
-    return new Promise(function(resolve, reject) {
+  handleIntent: function handleIntent(_ref3) {
+    var context = _ref3.context;
+    var entities = _ref3.entities;
+
+    return new Promise(function (resolve, reject) {
       var resultPair = intentHandler.handleIntent(context, entities);
-      if(resultPair.result == intentHandler.RESULTS.FAILURE){
+      if (resultPair.result == intentHandler.RESULTS.FAILURE) {
         console.error("There was an error when handling intent: " + entities.intent);
         resultPair.context.result = "No functions handled this question.";
       }
       return resolve(resultPair.context);
     });
-  },
+  }
 };
 
 // Setting up our bot
 var wit = new Wit({
   accessToken: WIT_TOKEN,
-  actions,
+  actions: actions,
   logger: new log.Logger(log.INFO)
 });
 
 // generic function sending messages
 function sendMessage(recipientId, message) {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: {
+      recipient: { id: recipientId },
+      message: message
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
 };
 
 // handler receiving messages
 app.post('/webhook', function (req, res) {
-    // Parse the Messenger payload
+  // Parse the Messenger payload
   // See the Webhook reference
   // https://developers.facebook.com/docs/messenger-platform/webhook-reference
   var data = req.body;
 
   if (data.object === 'page') {
-    data.entry.forEach(function(entry) {
-      entry.messaging.forEach(function(event) {
+    data.entry.forEach(function (entry) {
+      entry.messaging.forEach(function (event) {
         if (event.message) {
           // Yay! We got a new message!
           // We retrieve the Facebook user ID of the sender
@@ -133,23 +135,24 @@ app.post('/webhook', function (req, res) {
           var sessionId = findOrCreateSession(sender);
 
           // We retrieve the message content
-          var {text, attachments} = event.message;
+          var _event$message = event.message;
+          var text = _event$message.text;
+          var attachments = _event$message.attachments;
+
 
           if (attachments) {
             // We received an attachment
             // Let's reply with an automatic message
-            sendMessage(sender, 'Sorry I can only process text messages for now.')
-            .catch(console.error);
+            sendMessage(sender, 'Sorry I can only process text messages for now.').catch(console.error);
           } else if (text) {
             // We received a text message
 
             // Let's forward the message to the Wit.ai Bot Engine
             // This will run all actions until our bot has nothing left to do
-            wit.runActions(
-              sessionId, // the user's current session
-              text, // the user's message
-              sessions[sessionId].context // the user's current session state
-            ).then((context) => {
+            wit.runActions(sessionId, // the user's current session
+            text, // the user's message
+            sessions[sessionId].context // the user's current session state
+            ).then(function (context) {
               // Our bot did everything it has to do.
               // Now it's waiting for further messages to proceed.
               console.log('Waiting for next user messages');
@@ -163,10 +166,9 @@ app.post('/webhook', function (req, res) {
 
               // Updating the user's current session state
               sessions[sessionId].context = context;
-            })
-            .catch((err) => {
+            }).catch(function (err) {
               console.error('Oops! Got an error from Wit: ', err.stack || err);
-            })
+            });
           }
         } else {
           console.log('received event', JSON.stringify(event));
@@ -177,5 +179,4 @@ app.post('/webhook', function (req, res) {
   res.sendStatus(200);
 });
 
-
-app.listen((process.env.PORT || 3000));
+app.listen(process.env.PORT || 3000);
